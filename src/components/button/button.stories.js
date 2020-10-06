caca588d7921ca6676bcc342b459de8c9979f835
Frenzy265 <klaus_franziska@gmx.de>
2020-10-06 15:19:53 +0200

    Finish 3 buttons

--- src/components/button/button.stories.js
+++ src/components/button/button.stories.js
@@ -1,8 +1,14 @@
 import "./button.css";
 import buttonText from "./button-text.html";
 import buttonLogo from "./button-logo.html";
+import buttonShare from "./button-share.html";
+import buttonPlay from "./button-play.html";
+import buttonBigContent from "./button-big-content.html";
 
 export default { title: "Components/Button" };
 
 export const withText = () => buttonText;
 export const withLogo = () => buttonLogo;
+export const withShare = () => buttonShare;
+export const withPlay = () => buttonPlay;
+export const withBigContent = () => buttonBigContent;
