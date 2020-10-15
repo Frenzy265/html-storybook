3dd0dc57336174dc0bd34d10c9a0d8277343716b
Frenzy265 <klaus_franziska@gmx.de>
2020-10-15 15:41:40 +0200

    Add utils with element.js and optimize code in track.js

--- src/components/track/track.js
+++ src/components/track/track.js
@@ -1,64 +1,115 @@
-import playAction from "../../assets/playAction.svg";
-import pauseBtn from "../../assets/icon-pause-gradient.svg";
+import playActionSrc from "../../assets/playAction.svg";
+import pauseBtnSrc from "../../assets/icon-pause-gradient.svg";
 import "./track.css";
+import { createElement } from "../../utils/element";
 
-export function createTrackElement(track) {
-  const trackLine = document.createElement("div");
-  trackLine.className = "track";
-
-  const imgElement = document.createElement("img");
-  imgElement.src = track.imgSrc;
-  imgElement.alt = `Image of ${track.artist}`;
-  imgElement.className = "track__image";
-
-  const titleElement = document.createElement("h3");
-  titleElement.innerText = track.title;
-
-  const artistElement = document.createElement("p");
-  artistElement.innerText = track.artist;
-
-  const musicText = document.createElement("div");
-  musicText.className = "textbox";
-
-  const buttonElement = document.createElement("button");
-  buttonElement.className = "btn__play";
-  const playActionElement = document.createElement("img");
-  playActionElement.src = playAction;
-  playActionElement.alt = "Image of Play Button";
-  buttonElement.append(playActionElement);
-
-  let isPlaying = false;
-
+export const createTrackElement = (track) => {
   const audioElement = new Audio(track.audioSrc);
-  buttonElement.onclick = function () {
-    if (isPlaying) {
-      // isPlaying = false;
-      audioElement.pause();
-      showActionBtn(playActionElement);
-    } else {
-      // isPlaying = true;
-      audioElement.play();
-      showPauseBtn(playActionElement);
-    }
-    isPlaying = !isPlaying;
-  };
+  const playActionElement = createElement("img", {
+    src: playActionSrc,
+  });
 
-  const ruleElement = document.createElement("div");
-  ruleElement.className = "rule";
-  ruleElement.append(musicText, buttonElement);
+  const trackElement = createElement("div", {
+    className: "track",
+    children: [
+      createElement("img", {
+        className: "track__image",
+        src: track.imgSrc,
+        alt: `Image of ${track.artist}`,
+      }),
 
-  musicText.append(titleElement, artistElement);
-  trackLine.append(imgElement, ruleElement);
+      createElement("div", {
+        className: "rule",
+        children: [
+          createElement("div", {
+            className: "textbox",
+            children: [
+              createElement("h3", {
+                innerText: track.title,
+              }),
+              createElement("p", {
+                innerText: track.artist,
+              }),
+            ],
+          }),
 
-  return trackLine;
-}
+          createElement("button", {
+            className: "btn__play",
+            children: [playActionElement],
+            onclick: () => {
+              if (!audioElement.paused) {
+                audioElement.pause();
+                showActionBtn(playActionElement);
+              } else {
+                audioElement.play();
+                showPauseBtn(playActionElement);
+              }
+            },
+          }),
+        ],
+      }),
+    ],
+  });
+
+  return trackElement;
+};
 
 const showActionBtn = (element) => {
-  element.src = playAction;
+  element.src = playActionSrc;
   element.alt = "Play Button";
 };
 
 const showPauseBtn = (element) => {
-  element.src = pauseBtn;
+  element.src = pauseBtnSrc;
   element.alt = "Pause Button";
 };
+
+//   const trackLine = document.createElement("div");
+//   trackLine.className = "track";
+
+//   const imgElement = document.createElement("img");
+//   imgElement.src = track.imgSrc;
+//   imgElement.alt = `Image of ${track.artist}`;
+//   imgElement.className = "track__image";
+
+//   const titleElement = document.createElement("h3");
+//   titleElement.innerText = track.title;
+
+//   const artistElement = document.createElement("p");
+//   artistElement.innerText = track.artist;
+
+//   const musicText = document.createElement("div");
+//   musicText.className = "textbox";
+
+//   const buttonElement = document.createElement("button");
+//   buttonElement.className = "btn__play";
+//   const playActionElement = document.createElement("img");
+//   playActionElement.src = playAction;
+//   playActionElement.alt = "Image of Play Button";
+//   buttonElement.append(playActionElement);
+
+//   let isPlaying = false;
+
+//   const audioElement = new Audio(track.audioSrc);
+//   buttonElement.onclick = function () {
+//     if (isPlaying) {
+//       // isPlaying = false;
+//       audioElement.pause();
+//       showActionBtn(playActionElement);
+//     } else {
+//       // isPlaying = true;
+//       audioElement.play();
+//       showPauseBtn(playActionElement);
+//     }
+//     isPlaying = !isPlaying;
+//   };
+
+//   const ruleElement = document.createElement("div");
+//   ruleElement.className = "rule";
+//   ruleElement.append(musicText, buttonElement);
+
+//   musicText.append(titleElement, artistElement);
+//   trackLine.append(imgElement, ruleElement);
+
+//   return trackLine;
+// }
