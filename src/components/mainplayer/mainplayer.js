4f1e0d238b1c9f5bb016f55c7dbb7523a7434c2a
Frenzy265 <klaus_franziska@gmx.de>
2020-10-16 12:09:18 +0200

    Add mainplayer

--- src/components/mainplayer/mainplayer.js
+++ src/components/mainplayer/mainplayer.js
@@ -1,6 +1,7 @@
 import "./mainplayer.css"
 import backBtnSrc from "../../assets/btn-back.svg"
 import playBtnSrc from "../../assets/btn-play-white.svg"
+import forBtnSrc from "../../assets/btn-forward.svg"
 
 export const createControlElement = (control) => {
 
@@ -8,36 +9,38 @@ const controlElement = createElement("div", {
 className: "control-box",
 children: [
     createElement("button", {
-        className: "btn-direction",
-     children: [
+        className: "btn__direction",
+        children: [
          createElement("img", {
              src: backBtnSrc,
-             alt: "Image of backward button"
-         })
-     ]  
-    })
+             alt: "Image of backward button",
+             })
+        ],  
+    }),
     createElement("button", {
-        className: "btn-play",
+        className: "btn__play",
         children: [
             createElement("img", {
                 src: playBtnSrc,
-                alt: "Image of play button"
+                alt: "Image of play button",
             })
-        ]
-    })
+        ],
+    }),
+    createElement("button", {
+         className: "btn__direction"
+         children: [
+            createElement("img", {
+            src: forBtnSrc,
+            alt: "Image of forward button",
+            })
+        ],
+}),
 
+],
 
-]
+},
 
+),
 
-}
-
-
-
-)
-
-
-
-
-
+return controlElement;
 }
